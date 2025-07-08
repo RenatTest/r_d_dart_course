352fe46a608f94d1552012ebe704c6d9844b4e8f
RenatTest <renattesttask@gmail.com>
2025-07-08 14:00:53 +0300

    feat(DART-11-homework): future-task_2

--- lib/home_work_11/future/hw_futures.dart
+++ lib/home_work_11/future/hw_futures.dart
@@ -1,7 +1,10 @@
 void main() async {
   // 1
-  String result = await fetchName();
-  print(result);
+  String resultName = await fetchName();
+  print(resultName);
+  // 2
+  String resultAge = await fetchAge();
+  print(resultAge);
 }
 
 // Task 1: Асинхронне отримання імені
@@ -13,5 +16,32 @@ void main() async {
 Future<String> fetchName() async {
   String name = 'Renat';
   await Future.delayed(Duration(seconds: 2));
-  return 'My name is $name';
+  return 'Мене звати $name';
+}
+
+// Task 2: Асинхронне отримання віку
+
+// Створіть метод Future<String> fetchAge(), який імітує асинхронну операцію.
+// Цей метод повинен повертати рядок "25" через 1500 мілісекунд.
+// Виведіть результат у консоль у форматі: "Мені [ваш вік] років".
+// Реалізуйте логіку, щоб слово "рік" змінювалося відповідно до значення віку (наприклад: 21 рік, 22 роки, 25 років).
+
+Future<String> fetchAge() async {
+  int age = 39;
+  await Future.delayed(Duration(milliseconds: 1500));
+  int value = age % 10;
+  int twoValues = age % 100;
+
+  if (twoValues >= 11 && twoValues <= 14) {
+    return 'Мені $age років';
+  }
+
+  switch (value) {
+    case 1:
+      return 'Мені $age рік';
+    case 2 || 3 || 4:
+      return 'Мені $age роки';
+    default:
+      return 'Мені $age років';
+  }
 }
